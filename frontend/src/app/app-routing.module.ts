f4096f3d7d41de994be53ebfc4dd1f5b469cde92
Rolando Corral <cyberkrane@gmail.com>
2024-06-03 13:02:32 -0300

    ruteo lazyloading

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:'', loadChildren: () => import('./Entry/entry.module').then(m => m.EntryModule)}
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, 
+  { path: 'login', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'entry', loadChildren: () => import('./Entry/entry.module').then(m => m.EntryModule) },
+  { path: '**', redirectTo: 'login' } 
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,9 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { EgressModule } from './Egress/egress.module';
-import { EntryModule } from './Entry/entry.module';
-import { ProcessingModule } from './Processing/processing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -17,9 +14,6 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     AppRoutingModule,
     BrowserModule,
-    EgressModule,
-    EntryModule,
-    ProcessingModule,
     SharedModule,
     HttpClientModule,
     ReactiveFormsModule
